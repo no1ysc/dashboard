f8f45d8660a005c05f668419c0d1dde9bcd87817
Seungchul Lee <num1ysc@gmail.com>
2020-01-07 01:25:54 +0900

    Create setup-docker.sh

---
+++ setup-docker.sh
@@ -0,0 +1,17 @@
+echo "amazon-linux-extras enable docker"
+amazon-linux-extras enable docker
+
+echo "amazon-linux-extras install docker -y"
+amazon-linux-extras install docker -y
+
+echo "sudo service docker start"
+sudo service docker start
+
+echo "sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+echo "sudo chmod +x /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose"
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
