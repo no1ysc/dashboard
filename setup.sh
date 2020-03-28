6c5eb35713c034ef264761a8e02ef95d4d67c068
Lucas <lucas@forspacelab.com>
2020-03-29 05:41:17 +0900

    5주차 준비

---
+++ setup.sh
@@ -0,0 +1,23 @@
+echo "amazon-linux-extras enable docker"
+amazon-linux-extras enable docker
+
+echo "amazon-linux-extras install docker -y"
+amazon-linux-extras install docker -y
+
+echo "sudo service docker start"
+sudo service docker start
+
+echo "sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+echo "sudo chmod +x /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose"
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+echo "amazon-linux-extras enable java-openjdk11"
+amazon-linux-extras enable java-openjdk11
+
+echo "amazon-linux-extras install java-openjdk11 -y"
+amazon-linux-extras install java-openjdk11 -y
